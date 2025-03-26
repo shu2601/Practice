c27f0df5bda19b6e9b0425ffb5e0f3bcd131b399
FenSh002 <shubhalfengade060@gmail.com>
2025-03-26 16:56:34 +0530

    Update array

--- src/Array/ItrateArry.java
+++ src/Array/ItrateArry.java
@@ -3,5 +3,9 @@ package Array;
 public class ItrateArry {
     public static void main(String[] args) {
 
+        int []a={1,2,3,4,5};
+        for(int i:a)
+            System.out.println(i);
+
     }
 }
